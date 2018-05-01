8137b9e355e7833d2bb8c15b8d002e25bf396def
maltaesousa <sos@ylb.ch>
2018-05-01 17:11:10 +0200

    typo

--- vialibera/settings.py
+++ vialibera/settings.py
@@ -33,7 +33,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'enrolments.apps.EnrolmentsConfig'
+    'enrolments.apps.EnrolmentsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
