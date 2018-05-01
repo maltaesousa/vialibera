40ecdfabc776b4b3eeeb38263772d747036095b4
maltaesousa <sos@ylb.ch>
2018-05-01 17:04:18 +0200

    Added models

--- checkin/models.py
+++ checkin/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+class Course(models.Model):
+  name = models.TextField
+  description = models.TextField
+  price = models.FloatField
+  date_start = models.DateTimeField
+  date_ends = models.DateTimeField
+
+class Student(models.Model):
+  firstname = models.TextField
+  lastname = models.TextField
+  post_address = models.TextField
+  post_code = models.IntegerField
+  post_town = models.TextField
+  email = models.EmailField
+  phone = models.IntegerField
+  birthdate = models.DateField
+
+class Enrolment(models.Model):
+  course = models.ForeignKey(Course, on_delete=models.CASCADE)
+  student = models.ForeignKey(Student, on_delete=models.CASCADE)
+  paid = models.BooleanField
