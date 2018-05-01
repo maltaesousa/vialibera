6d667791104d06a3de8829970b52dcf0904bcde4
maltaesousa <sos@ylb.ch>
2018-05-01 17:13:35 +0200

    enrolments urls

--- vialibera/urls.py
+++ vialibera/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('checkin/', include('checkin.urls')),
+    path('enrolments/', include('enrolments.urls')),
     path('admin/', admin.site.urls),
 ]
