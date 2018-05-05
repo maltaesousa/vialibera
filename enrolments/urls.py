095f85b07fd90314f8413049d6ef81f08ff2493d
maltaesousa <sos@ylb.ch>
2018-05-05 23:38:34 +0200

    rename enrolments list

--- enrolments/urls.py
+++ enrolments/urls.py
@@ -6,6 +6,6 @@ app_name = 'enrolments'
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
     path('<int:pk>/', views.DetailView.as_view(), name='detail'),
-    path('<int:pk>/enrolments/', views.ResultsView.as_view(), name='enrolments'),
+    path('<int:pk>/enrolments/', views.EnrolmentsView.as_view(), name='enrolments'),
     path('<int:course_id>/enrol/', views.enrol, name='enrol'),
 ]
