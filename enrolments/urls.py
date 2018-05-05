755633ccf110949c2f1f9469f313599b39e0ab38
maltaesousa <sos@ylb.ch>
2018-05-05 17:59:50 +0200

    add urls and views

--- enrolments/urls.py
+++ enrolments/urls.py
@@ -4,4 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('<int:course_id>/', views.detail, name='detail'),
+    path('<int:course_id>/enrolments/', views.enrolments, name='enrolments'),
+    path('<int:course_id>/enrol/', views.enrol, name='enrol'),
 ]
--- enrolments/views.py
+++ enrolments/views.py
@@ -1,7 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import get_list_or_404, get_object_or_404, render
 from django.http import HttpResponse
+
+from .models import Course, Enrolment, Student
 
 import os
 
 def index(request):
-    return HttpResponse('Working')
+    latest_course_list = Course.objects.order_by('-id')[:5]
+    context = {
+        'latest_course_list': latest_course_list
+    }
+    return render(request, 'enrolments/index.html', context)
+
+def detail(request, course_id):
+    course = get_object_or_404(Course, pk=course_id)
+    return render(request, 'enrolments/detail.html', {'course': course})
+
+def enrolments(request, course_id):
+    enrolments_list = get_list_or_404(Enrolment, course_id=course_id)
+    return render(request, 'enrolments/enrolments.html', {'enrolments_list': enrolments_list})
+
+def enrol(request, course_id):
+    return render(request, 'enrolments/enrol.html', {})
